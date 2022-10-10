dcdf46e9f770bc2d7fc8b06e40dcfc3a71b02e00
j.perrochaud <j.perrochaud@student.isartdigital.com>
2022-10-10 12:03:41 +0200

    modified:   .gitignore 	modified:   src/m_allocator.c 	modified:   src/metadata.h

--- src/m_allocator.c
+++ src/m_allocator.c
@@ -2,7 +2,24 @@
 #include <m_allocator.h>
 #include "metadata.h"
 
-static Metadata* metadata = NULL;
+static Metadata *metadata = NULL;
+
+Metadata *get_free_block(size_t size)
+{
+   if (metadata != NULL)
+   {
+      while (metadata->blockSize < size)
+      {
+         if (metadata->isOccupied == 0)
+            return metadata;
+         if (metadata->next != NULL)
+            metadata = metadata->next;
+         else
+            return NULL;
+      }
+   }
+   return NULL;
+}
 
 void *malloc_hook(size_t size, const void *caller) { return m_malloc(size); }
 void *realloc_hook(void *ptr, size_t size, const void *caller) { return m_realloc(ptr, size); }
@@ -15,17 +32,36 @@ void m_setup_hooks(void)
    __free_hook = free_hook;
 }
 
-void *m_malloc(size_t size) 
+void *m_malloc(size_t size)
 {
-   return (void *)sbrk(size);
+   metadata = get_free_block(size);
+   if (metadata == NULL)
+   {
+      Metadata newMetadata;
+      newMetadata.adr = sbrk(size);
+      newMetadata.blockSize = size;
+      newMetadata.isOccupied = 1;
+      if (metadata != NULL)
+      {
+         metadata->next = &newMetadata;
+         metadata = metadata->next;
+      }
+      else
+         metadata = &newMetadata;
+   }
+   else
+      metadata->isOccupied = 1;
+   return metadata->adr;
 }
 
 void *m_realloc(void *ptr, size_t size)
 {
+   return 0;
 }
 
 void *m_calloc(size_t nb, size_t size)
 {
+   return 0;
 }
 
 void m_free(void *ptr)
--- src/metadata.h
+++ src/metadata.h
@@ -1,6 +1,12 @@
+
 typedef struct Metadata
 {
    // Add metadata information needed
    // e.g. size of the block, pointer, free/occupied
-   struct Metadata* next;
+   int *adr;
+   size_t blockSize;
+   _Bool isOccupied;
+   struct Metadata *next;
 } Metadata;
+
+Metadata* get_free_block(size_t size);
